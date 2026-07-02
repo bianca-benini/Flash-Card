4bc06d03f7122c079c62476db118c4ab33aade8b
bianca-benini <bianca.benini@escola.pr.gov.br>
2026-07-02 12:20:36 +0000

    .

--- perguntas.js
+++ perguntas.js
@@ -2,19 +2,19 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'Filosofia',
+    'Qual é o conceito de etica?',
+    'conjunto de principios que orienta o comportamento homano, indicando oque é moralmente correto ou errado'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'Filosofia',
+    'Como alcançar a felicidade no cinismo?',
+    'Por meio da virtude e de uma vida em harmonia com a natureza'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'Programação',
+    'O que é uma linguagem de programação?',
+    'É uma lingua usada para escrever códigos que permitem o computador executar tarefas'
 )
